21208cf9d28a4656b7d1254d42c7285da51094d9
mohit564 <86644125+mohit564@users.noreply.github.com>
2023-02-26 19:20:15 +0530

    added logic to show all possible moves for knight when we click on cell
    Added logic to show all possible moves for knight when we click on chessboard cell
    
    Possible moves for knight are shown in green color

--- src/components/ChessBoard/index.jsx
+++ src/components/ChessBoard/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 // styles
 import "./ChessBoard.css";
@@ -11,15 +11,52 @@ import { xAxis, yAxis } from "../../constants";
 import { setRole } from "../../utils/chessboard";
 
 const ChessBoard = () => {
+  const [knightPossibleMoves, setKnightPossibleMoves] = useState([]);
+
+  // Check if move is valid or not on chessboard
+  const testSquare = (x, y, moves) => {
+    if (x > 8 || x < 1 || y > 8 || y < 1) return;
+    moves.push({ x, y });
+  };
+
+  const getPossibleMovesForKnight = (x, y) => {
+    let possibleMoves = [];
+
+    // Top side possible moves
+    testSquare(x - 1, y + 2, possibleMoves);
+    testSquare(x + 1, y + 2, possibleMoves);
+
+    // Bottom side possible moves
+    testSquare(x - 1, y - 2, possibleMoves);
+    testSquare(x + 1, y - 2, possibleMoves);
+
+    // Left side possible moves
+    testSquare(x - 2, y - 1, possibleMoves);
+    testSquare(x - 2, y + 1, possibleMoves);
+
+    // Right side possible moves
+    testSquare(x + 2, y - 1, possibleMoves);
+    testSquare(x + 2, y + 1, possibleMoves);
+
+    setKnightPossibleMoves(possibleMoves);
+  };
+
   return (
     <div className="chessboard">
       {yAxis.map((row) => {
         return xAxis.map((column, columnIdx) => (
           <div
             key={`${column}${row}`}
-            className="chessboard-square"
+            className={`chessboard-square ${
+              knightPossibleMoves.some(
+                (pos) => pos.y === row && pos.x === columnIdx + 1
+              )
+                ? "possible-move"
+                : ""
+            }`}
             role={setRole(row, columnIdx)}
             data-position={`${column}${row}`}
+            onClick={() => getPossibleMovesForKnight(columnIdx + 1, row)}
           >
             <ChessBoardPiece row={row} column={column} />
           </div>
